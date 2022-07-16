15d16e6d5ef82e42f5f21d665752fbe5e3eec0a1
Aleksander <aleksandergorecki80@gmail.com>
2022-07-16 18:54:27 +0200

    Linear search

--- linear/01_intro.js
+++ linear/01_intro.js
@@ -1,14 +1,12 @@
-const number = 55;
 const arr = [11,55,4,0,1,8]
 const linearSearch = (arr,number) => {
-    let result;
     for(let i=0; i<arr.length; i++){
-        if(arr[i] === number){
-            return result = i
-        }
-        result = -1;
+        if(arr[i] === number) return i;
     }
-    return result
+    return -1
 }
 
-console.log(linearSearch(arr, number))
+console.log(linearSearch(arr, 11))
+console.log(linearSearch(arr, 8))
+console.log(linearSearch(arr, 4))
+console.log(linearSearch(arr, 99))
